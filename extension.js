87b490bc36d61437c2bbd6a404d6dc93a32f0fe8
Payton Swick <payton@foolord.com>
2020-08-15 00:57:09 -0400

    Add GrepDef for word under cursor

--- extension.js
+++ extension.js
@@ -8,7 +8,7 @@ const exec = util.promisify(childProcess.exec);
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	let disposable = vscode.commands.registerCommand(
+	const grepdefCommand = vscode.commands.registerCommand(
 		'vscode-grepdef.grepdef',
 		async () => {
 			const symbol = await vscode.window.showInputBox({
@@ -30,7 +30,27 @@ function activate(context) {
 		}
 	);
 
-	context.subscriptions.push(disposable);
+	const grepdefWordCommand = vscode.commands.registerCommand(
+		'vscode-grepdef.grepdefword',
+		async () => {
+			const symbol = getWordUnderCursor();
+			const results = await runGrepDef(symbol);
+			const selectedFile = await vscode.window.showQuickPick(
+				results.split(/\n/)
+			);
+			if (selectedFile) {
+				const resultParts = selectedFile.split(':');
+				if (resultParts.length === 3) {
+					const fileName = resultParts[0];
+					const lineNumber = parseInt(resultParts[1]);
+					openFileAtLine(fileName, lineNumber);
+				}
+			}
+		}
+	);
+
+	context.subscriptions.push(grepdefCommand);
+	context.subscriptions.push(grepdefWordCommand);
 }
 exports.activate = activate;
 
@@ -53,9 +73,9 @@ async function runGrepDef(symbol) {
 }
 
 /**
- * 
- * @param {string} fileName 
- * @param {number} lineNumber 
+ *
+ * @param {string} fileName
+ * @param {number} lineNumber
  */
 async function openFileAtLine(fileName, lineNumber) {
 	const document = await vscode.workspace.openTextDocument(fileName);
@@ -64,6 +84,20 @@ async function openFileAtLine(fileName, lineNumber) {
 	const range = editor.document.lineAt(lineNumber - 1).range;
 	editor.selection = new vscode.Selection(range.start, range.end);
 	editor.revealRange(range);
+}
+
+/**
+ * @returns {string|null}
+ */
+function getWordUnderCursor() {
+	const editor = vscode.window.activeTextEditor;
+	let wordRange = editor.document.getWordRangeAtPosition(
+		editor.selection.start
+	);
+	if (!wordRange) {
+		return null;
+	}
+	return editor.document.getText(wordRange);
 }
 
 module.exports = {
